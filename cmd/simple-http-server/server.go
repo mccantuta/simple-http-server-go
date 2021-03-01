ce582e93dd786ee62cfa0689be3a86b9e9c7fb93
Miguel Ccantuta <mccantuta@yahoo.com>
2021-03-01 01:19:46 -0500

    updating

--- cmd/simple-http-server/server.go
+++ cmd/simple-http-server/server.go
@@ -6,11 +6,24 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Testing")
+type Customer struct {
+	Id   string
+	Name string
+}
+
+func customerHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintf(w, "Testing GET")
+	case http.MethodPost:
+		fmt.Fprintf(w, "Testing POST")
+	default:
+		fmt.Fprintf(w, "Testing")
+	}
+
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/customer", customerHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
